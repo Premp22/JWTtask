79b93f1caa5bc7b9b05f79b96c9ec692ef018aa8
prem <prem@bititude.com>
2021-09-07 18:16:31 +0530

    delete corrected

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,5 @@ router.post("/users", controller.CreateUser);
 router.get("/users", controller.listAllUsers);
 router.get("/users/:userId", controller.listOneUser);
 router.patch("/users/:userId", controller.updateUser);
-
+router.delete("/users/:userId", controller.deleteUser);
 module.exports = router;
